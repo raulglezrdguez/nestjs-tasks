c791a9bb58f9ba714e88248d183b65095f0bc718
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-10 15:47:28 -0500

    module auth: controller, service and repository created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,7 +12,6 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
