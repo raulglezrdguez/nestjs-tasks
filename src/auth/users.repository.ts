ca15076df756902671f12b94c53037fb8bd1963e
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-10 16:15:38 -0500

    auth dto created

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,20 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    let user = this.create({
+      username,
+      password,
+    });
+    user = await this.save(user);
+
+    if (!user) {
+      throw new InternalServerErrorException(`Error creating user, try again`);
+    }
+  }
+}
