141d4fad7e93d59d9ab7232606c06726e1f57d5c
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-11 15:47:45 -0500

    auth credentials dto validated

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
