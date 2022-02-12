ba2d97d608e65daa22c2be62a33d082ec7fe0455
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-12 17:32:17 -0500

    configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,40 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { configValidationSchema } from 'config.schema';
 
+// const configuration = () => ({
+//   DB_TYPE: 'postgres',
+//   DB_HOST: 'localhost',
+//   DB_PORT: 5432,
+//   DB_USERNAME: 'postgres',
+//   DB_PASSWORD: 'postgres',
+//   DB_DATABASE: 'tasks',
+// });
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // validationSchema: configValidationSchema, // this dont work
+      // envFilePath: [`.env.stage.${process.env.STAGE}`],
+      // load: [configuration],
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'tasks',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModule> => ({
+        type: configService.get('DB_TYPE'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     AuthModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
